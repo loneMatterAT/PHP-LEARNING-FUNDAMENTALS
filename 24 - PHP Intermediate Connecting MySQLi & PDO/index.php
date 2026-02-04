fc26a757e64028e67443fbe134e601bba70b3750
Houtarou Oreki <oreki.houtarou01@gmail.com>
2026-02-04 21:34:10 +0800

    This is My 24th Introduction to Intermediate Series of Learning on How to Connect a PHP into MySQL phpMyAdmin in PHP Fundamentals.

---
+++ 24 - PHP Intermediate Connecting MySQLi & PDO/index.php
@@ -0,0 +1,29 @@
+
+<?php
+  // 1. MySQLi Extension
+  //  We use phpMyAdmin for this lesson, using this allows us to configure
+  //  database (eg. SQL queries, monitor status, etc.)
+
+  // We use include() function to connect the database.php file 
+  include("database.php");
+
+  
+
+
+
+
+
+  // 2. PDP (PHP Data Objects) Intermediate
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Intermediate Connecting MySQLi & PDO | 24</title>
+</head>
+<body>
+  Hello <br>
+</body>
+</html>
