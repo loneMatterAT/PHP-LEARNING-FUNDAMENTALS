b5679fca7d8df950a78f6421a4ff78ffb80f5ce2
Houtarou Oreki <oreki.houtarou01@gmail.com>
2026-02-06 17:39:52 +0800

    This is My 25th Intermediate Series of Learning on How to Create a Table in phpMyadmin in PHP Fundamentals.

---
+++ 25 - PHP Intermediate Creating Table in phpMyadmin/table.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Intermediate Creating Table in phpMyadmin | 25</title>
+</head>
+<body>
+  
+</body>
+</html>
+
+<?php
+
+  // Preview of Creating Table using phpmyadmin sample MySQL queries
+  // CREATE TABLE `businessdb`.`admins` (`id` INT NOT NULL
+  // AUTO_INCREMENT , `admin` VARCHAR(100) NOT NULL , `password
+  // ` CHAR NOT NULL , `reg_date` DATETIME NOT NULL DEFAULT
+  //  CURRENT_TIMESTAMP , PRIMARY KEY (`id`), UNIQUE (`admin`))
+  //  ENGINE = InnoDB;
+
+?>
